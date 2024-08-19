c644437028d7e21eb802f1fdc394a314fc728576
Skilsar <skilsar@gmail.com>
2024-08-20 00:03:31 +0300

    Fixed structure

--- index.js
+++ index.js
@@ -74,5 +74,5 @@ const compareImage = async (twitterProfilePicURL, assetCDNURL) => {
 };
 
 loadPixelmatch().then(() => {
-  compareImage("./img/kik.png", "./img/askfm.png");
+  compareImage("./img/KYSk5JXjdeh.png", "./img/kik.png");
 });
